1e4c1d7a810ec002d587b2ec5476409166c87e70
Gary Kwong [:gkw] <nth10sd@gmail.com>
2021-10-07 16:42:42 -0700

    Prepare first 1.0.0 release

--- innanzi/__init__.py
+++ innanzi/__init__.py
@@ -1,7 +1,7 @@
 """Module details."""
 
 __title__ = "Innanzi"
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 __author__ = "Gary Kwong"
 __license__ = "MIT"
 __copyright__ = f"Copyright 2021 {__author__}"
