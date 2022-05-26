351ba5cefb37ad555de8b4d5716c09730e598748
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-05-25 21:42:27 -0700

    Also bump pandas to 1.4.2

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
         ],
     },
     install_requires=[  # Include relevant types-* package, e.g. types-toml & toml
-        "pandas ~= 1.3.3",
+        "pandas ~= 1.4.2",
         "pandas-stubs ~= 1.2.0.58",
         "toml ~= 0.10.2",
         "setuptools >= 60.0.5",
