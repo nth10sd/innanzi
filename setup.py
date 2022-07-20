7cf8a14814993f13059f470aac021c2bb02b9276
Gary Kwong [:gkw] <nth10sd@gmail.com>
2022-07-20 07:34:20 -0700

    Bump mypy to 0.971 along with corresponding stub packages

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ EXTRAS = {
         "flake8-typing-imports ~= 1.12.0",
         "flynt ~= 0.76",
         "isort ~= 5.10.1",
-        "mypy==0.961",
+        "mypy==0.971",
         "pep8-naming ~= 0.13.0",
         "pylint ~= 2.14.1",
         "pytest ~= 7.1.2",
@@ -51,7 +51,7 @@ EXTRAS = {
         "pytest-cov ~= 3.0.0",
         "pytest-dependency ~= 0.5.1",
         "pytest-flake8 ~= 1.1.0",
-        "pytest-mypy ~= 0.9.0",
+        "pytest-mypy ~= 0.9.1",
         "pytest-pylint ~= 0.18.0",
         "pyupgrade-directories ~= 0.2.0",
         "sphinx ~= 5.0.1",
@@ -81,10 +81,10 @@ setup(
     },
     install_requires=[  # Include relevant types-* package, e.g. types-toml & toml
         "pandas ~= 1.4.2",
-        "pandas-stubs==1.2.0.58",
+        "pandas-stubs==1.4.3.220718",
         "toml ~= 0.10.2",
         "setuptools >= 60.0.5",
-        "types-setuptools==57.4.16",  # Bump types-* only with mypy
+        "types-setuptools==63.2.0",  # Bump types-* only with mypy
         "types-toml==0.10.7",  # For (temp) infrastructure stuff of some packages
         "wheel >= 0.37.0",
     ],
